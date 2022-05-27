5545e1eee61607d575bde3b4baa5cb29b43c3ec3
Chatethakhun <chatethakhun@Chatethakhuns-Mac-mini.local>
2022-05-27 21:40:58 +0700

    fix size of profile image

--- components/profileImage.tsx
+++ components/profileImage.tsx
@@ -4,7 +4,7 @@ interface ProfileImageProps {
 
 const ProfileImage = ({ imageUrl }: ProfileImageProps) => {
     return <div className='img-profile'>
-        <div className='w-[30px] h-[30px] md:w-[60px] md:w-[60px] rounded-full bg-no-repeat bg-cover bg-center '
+        <div className='w-[30px] h-[30px] md:w-[60px] md:h-[60px] rounded-full bg-no-repeat bg-cover bg-center '
             style={{ backgroundImage: `url(${ imageUrl || 'https://source.unsplash.com/user/c_v_r'})` }}
         >
 
