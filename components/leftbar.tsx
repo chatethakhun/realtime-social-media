49ee73ad488be92083af088bade90c93282ab963
0Baht50 <chatethakhun.jack@gmail.com>
2022-05-31 11:08:54 +0700

    create active tab

--- components/leftbar.tsx
+++ components/leftbar.tsx
@@ -8,10 +8,10 @@ const LeftBar = () => {
     const { logout } = useAuth()
     const { confirm } = useConfrim()
     const router = useRouter()
-    return <div className="text-right pr-5 mt-5 flex flex-col gap-5 cursor-pointer">
-        <SideBarLink icon={faHome} label='Home' onClick={() => router.push('/')}/>
-        <SideBarLink icon={faUser} label='Profile' onClick={() => router.push('/profile')}/>
-        <SideBarLink icon={faPowerOff} label='Sign Out' onClick={async () =>  {
+    return <div className="text-right mt-5 flex flex-col gap-5 cursor-pointer">
+        <SideBarLink icon={faHome} label='Home' onClick={() => router.push('/')} active={router.pathname === '/'}/>
+        <SideBarLink icon={faUser} label='Profile' onClick={() => router.push('/profile')} active={router.pathname === '/profile'}/>
+        <SideBarLink icon={faPowerOff} active={false} label='Sign Out' onClick={async () =>  {
             const isConfirm = await confirm()
             if(isConfirm) {
                 logout()
--- components/sidebarlink.tsx
+++ components/sidebarlink.tsx
@@ -3,11 +3,12 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 interface SideBarLink {
     icon: any,
     label: string,
-    onClick: () => {}
+    onClick: () => {},
+    active: boolean
 }
 
-const SideBarLink = ({ icon, label, onClick }: SideBarLink) => {
-    return <div className="ml-1 flex gap-2 items-center md:justify-end lg:justify-start" onClick={onClick}>
+const SideBarLink = ({ icon, label, onClick, active  }: SideBarLink) => {
+    return <div className={`flex gap-2 px-3 py-2  items-center md:justify-end lg:justify-start ${active && 'bg-teal-500' }`} onClick={onClick}>
         <div className="basis-[100pxpx]"><FontAwesomeIcon icon={icon} className="text-white " /></div>
         <div className="text-white hidden lg:block text-base">{label}</div>
     </div>
