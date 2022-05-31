0703db772d8e0197b0c80e4585733e8f831a5f77
0Baht50 <chatethakhun.jack@gmail.com>
2022-05-31 11:48:19 +0700

    create hamburger button

--- components/container.tsx
+++ components/container.tsx
@@ -1,3 +1,7 @@
+import { faHamburger } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { useRef, useState } from "react"
+import useOnClickOutside from "../hooks/useOnClickOutside"
 import LeftBar from "./leftbar"
 
 interface ContainerProps {
@@ -5,12 +9,28 @@ interface ContainerProps {
 }
 
 const Container = ({ children }: ContainerProps) => {
+    const [openMenu, setOpenMenu] = useState(false)
+    const menuRef = useRef(null)
+
+    useOnClickOutside(menuRef, () => setOpenMenu(!openMenu))
+
     return <div className={`container m-auto`}>
         <div className='flex min-h-[100vh]'>
-            <div className='hidden md:block md:w-[50px]  lg:w-[200px]'>
+            <div className={`hidden md:block md:w-[50px]  lg:w-[200px]`}>
                 <LeftBar />
             </div>
             <div className='pt-9 border-x-2 border-teal-500 w-[100%] '> {children}</div>
+            {
+                    openMenu && <div className="absolute bottom-[80px] left-[20px]" ref={menuRef}>
+                        <LeftBar />
+                    </div>
+            }
+            <div className="block md:hidden absolute bottom-[20px] left-[20px]" >
+                <button className="bg-teal-500 px-3 py-2" onClick={() => setOpenMenu(!openMenu)}>
+                    <FontAwesomeIcon icon={faHamburger} />
+                </button>
+            </div>
+
         </div>
     </div>
 }
